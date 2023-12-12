e613b9f1505eb0a0f9be490c701e4969336ed6de
Wanyaxeli <eliwanyax@gmail.com>
2023-12-12 17:05:24 +0300

    added baseline

--- src/components/CreateAcnount.js
+++ src/components/CreateAcnount.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
+import { useOutletContext } from "react-router-dom";
 const CreateAccount=()=>{
+    const[details,setDatail]=useOutletContext()
+    const handleInput=(e)=>{
+    const  name=e.target.name
+    const value=e.target.value
+    setDatail({...details,[name]:value})
+    }
     return(
      <div className="w-full h-full shadow-md rounded-md p-3">
         <h1 className="mt-2">Create account</h1>
         <form>
-            <input type='text' className="w-full placeholder:text-black h-[30px] border-b border-b-black my-3 focus:outline-none" placeholder='First name' />
-            <input type='text' className="w-full h-[30px] border-b placeholder:text-black border-b-black my-3 focus:outline-none" placeholder='Last name' />
-            <input type='email' className="w-full h-[30px] border-b border-b-black my-3 placeholder:text-black focus:outline-none" placeholder='Email' />
-            <input type='password' className="w-full h-[30px] border-b border-b-black my-3 focus:outline-none placeholder:text-black" placeholder='Password' />
-            <input type='password' className="w-full h-[30px] border-b border-b-black my-3 focus:outline-none placeholder:text-black" placeholder='Confirm password' />
+            <input value={details.first_name} name='first_name'  onChange={handleInput} type='text' className="w-full placeholder:text-black h-[30px] border-b border-b-black my-3 focus:outline-none" placeholder='First name' />
+            <input value={details.last_name}  name='last_name' onChange={handleInput} type='text' className="w-full h-[30px] border-b placeholder:text-black border-b-black my-3 focus:outline-none" placeholder='Last name' />
+            <input value={details.email} name='email' onChange={handleInput} type='email' className="w-full h-[30px] border-b border-b-black my-3 placeholder:text-black focus:outline-none" placeholder='Email' />
+            <input value={details.password}  name='password' onChange={handleInput} type='password' className="w-full h-[30px] border-b border-b-black my-3 focus:outline-none placeholder:text-black" placeholder='Password' />
+            <input  value={details.confirm_password} name='confirm_password' onChange={handleInput} type='password' className="w-full h-[30px] border-b border-b-black my-3 focus:outline-none placeholder:text-black" placeholder='Confirm password' />
         </form>
     </div>
     )
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React ,{useState}from "react";
 import login from '../images/lognImg.png';
+import axios from "axios";
 import { Outlet ,useNavigate,useLocation} from "react-router-dom";
 const Login =()=>{
+    const initialState={
+        first_name:'',
+        last_name:'',
+        email:'',
+        password:'',
+        confirm_password:''
+    }
+    const [details,setDatail]=useState(initialState)
     const navigate=useNavigate()
     const location =useLocation()
     const{pathname}=location
-    console.log(pathname.split('/')[2])
     const splitPath=pathname.split('/')[2]
     const handleup=()=>{
         navigate('signup')
     }
     const handleIn=()=>{
         navigate('signin')
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        axios.post('http://127.0.0.1:8000/api/token/',details,{headers: 
+        {'Content-Type': 'application/json'}})
+        .then(response=>{
+           console.log(response.data)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+        setDatail(initialState)
     }
     return(
         <div className="py-2 mx-auto max-w-[90%] h-[92vh] ">
@@ -20,11 +40,11 @@ const Login =()=>{
                     <img src={login}className="w-full h-full object-contain"  ></img>
                 </div>
                 <div className="flex-[0.4] h-[80%] mt-6">
-                    <Outlet/>
+                    <Outlet context={[details,setDatail]}/>
                 </div>
             </div>
             <div className="w-[100%]  my-3 h-auto mx-auto">
-               <button className="w-full h-[50px] bg-yellow-500 hover:bg-yellow-400 rounded-md cursor-pointer text-white">Continue</button>
+               <button type="submit" onClick={handleSubmit} className="w-full h-[50px] bg-yellow-500 hover:bg-yellow-400 rounded-md cursor-pointer text-white">Continue</button>
             </div>
             <div className="text-center">
                 {splitPath === 'signup'?<p>Already acount?<span onClick={handleIn} className="text-yellow-500 cursor-pointer">sign in</span></p>:<p>Don't have an acount already?<span onClick={handleup} className="text-yellow-500 cursor-pointer">create</span></p>}
